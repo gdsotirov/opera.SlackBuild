7dad34c85c02509200d663510419bed4fda58584
Georgi D. Sotirov <gdsotirov@dir.bg>
2009-12-06 15:34:32 +0000

    Rename configuration files, colorize output

--- doinst.sh
+++ doinst.sh
@@ -1,3 +1,9 @@
+# Bash colors                                                                                                                                 
+C_GREEN=$'\e[32;01m'                                                                                                                          
+C_YELLOW=$'\e[33;01m'                                                                                                                         
+C_RED=$'\e[31;01m'                                                                                                                            
+C_NORMAL=$'\e[0m'
+
 config() {
   NEW="$1"
   OLD="`dirname $NEW`/`basename $NEW .new`"
@@ -11,14 +17,26 @@ config() {
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
-config etc/opera6rc.new
-config etc/opera6rc.fixed.new
+config etc/operaprefs.ini.new
+config etc/operaprefs_fixed.ini
 
 if [ -x usr/bin/update-desktop-database ]; then
+  echo -n "Updating desktop database... "
   usr/bin/update-desktop-database usr/share/applications >/dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    echo "${C_GREEN}DONE${C_NORMAL}"
+  else
+    echo "${C_RED}FAILURE${C_NORMAL}"
+  fi
 fi
 
 if [ -x usr/bin/update-mime-database ]; then
+  echo -n "Updating MIME database... "
   usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    echo "${C_GREEN}DONE${C_NORMAL}"
+  else
+    echo "${C_RED}FAILURE${C_NORMAL}"
+  fi
 fi
 
