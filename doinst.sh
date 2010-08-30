8d4d844dc2f29aa790df45ce5654104cdeb65e49
Georgi D. Sotirov <gdsotirov@dir.bg>
2010-08-30 17:41:54 +0000

    Remove configuration file swap

--- doinst.sh
+++ doinst.sh
@@ -4,22 +4,6 @@ C_YELLOW=$'\e[33;01m'
 C_RED=$'\e[31;01m'                                                                                                                            
 C_NORMAL=$'\e[0m'
 
-config() {
-  NEW="$1"
-  OLD="`dirname $NEW`/`basename $NEW .new`"
-  # If there's no config file by that name, mv it over:
-  if [ ! -r $OLD ]; then
-    mv $NEW $OLD
-  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then
-    # toss the redundant copy
-    rm $NEW
-  fi
-  # Otherwise, we leave the .new copy for the admin to consider...
-}
-
-config etc/operaprefs.ini.new
-config etc/operaprefs_fixed.ini
-
 if [ -x usr/bin/update-desktop-database ]; then
   echo -n "Updating desktop database... "
   usr/bin/update-desktop-database usr/share/applications >/dev/null 2>&1
